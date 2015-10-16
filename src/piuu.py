7d4c453654fe31f828ea685a4ac42f34ebbb5a98
Ian List <ian_alexander.list@mailbox.tu-dresden.de>
2015-10-16 10:27:51 +0200

    When executed saves image URL and delete-hash to /home/[user]/.images.txt

--- src/piuu.py
+++ src/piuu.py
@@ -1,3 +1,5 @@
+#! /bin/python3
+
 '''
 This is just a small python module I made to upload
 an image to imgur, which then copies the image link
@@ -6,9 +8,11 @@ directly into the systems clipboard
 
 from imgurpython import ImgurClient
 from sys import argv
+from getpass import getuser
 import pyperclip
 
 CLIENT = ImgurClient('5df57a2eb3ac87a', '')
+SAVEFILE = "/home/{0}/.images.txt".format(getuser())
 IMAGE_PATH = argv[-1]
 
 def upload_image(client, **kwargs):
@@ -33,8 +37,16 @@ def upload_image(client, **kwargs):
     image = client.upload_from_path(IMAGE_PATH, config=config, anon=True)
     return image
 
+def write_hash(image):
+    '''
+    This function writes the delete-hash to a file, so the user can check his uploaded images
+    or delete the image afterwards
+    '''
+    to_save = "Image-URL: {0}, Delete-Hash: {1}".format(image['link'], image['deletehash'])
+    with open(SAVEFILE, "a") as output:
+        print(to_save, file=output)
+
 if __name__ == '__main__':
     UPLOAD = upload_image(CLIENT)
     pyperclip.copy(UPLOAD['link'])
-    print(UPLOAD)
-
+    write_hash(UPLOAD)
